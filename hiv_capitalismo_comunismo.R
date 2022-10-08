cbbc915a0e0d5985665e249b9edaf6f2dafeafdd
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-10-07 21:35:26 -0300

    Add files via upload

--- hiv_capitalismo_comunismo.R
+++ hiv_capitalismo_comunismo.R
@@ -50,4 +50,21 @@ aids2 <- aids %>%
 
 # Gráficos ---------------------------------------------------------------------------------------------------------------------------------
 
+c4a("safe", 6)
 
+ggplot(aids1, aes(x = fct_reorder(Entity, media), 
+                  y = media, fill = Entity)) +
+  geom_col() +
+  geom_errorbar(aes(ymin = media - se, ymax = media + se),
+                size = 0.8, width = 0.2) +
+  scale_fill_manual(values = c("#88CCEE", "#CC6677",
+                               "#DDCC77", "#117733",
+                               "#332288", "#AA4499")) +
+  scale_y_continuous(expand = expansion(mult = c(0,0))) +
+  scale_x_discrete(labels = c("Japão", "Alemanha", "Coreia do Norte",
+                              "China", "Cuba", "Estados Unidos")) +
+  labs(x = "Países", y = "Porcentagem mortes por HIV") +
+  theme_ipsum(axis_title_size = 16,
+              axis_text_size = 14) +
+  theme(legend.position = "none",
+        axis.text = element_text(color = "black"))
